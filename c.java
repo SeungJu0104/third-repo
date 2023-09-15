73591ec60e30316542bcc7cfd71d836272a536e7
남승주 <145091038+SeungJu0104@users.noreply.github.com>
2023-09-15 14:27:46 +0900

    세번째 저장소 실습 수정
    자바 클래스 및 코드 수정

--- c.java
+++ c.java
@@ -1,1 +1,22 @@
-System.out.println("오늘은 비가 옵니다.");
+class weather {
+
+  public void rain(){
+    
+  System.out.println("오늘은 비가 옵니다.");
+  }
+
+}
+
+class testMain {
+  public static void main(String[] args){
+    weather n1 = new weather();
+    n1.rain();
+    
+
+
+  }
+}
+
+
+
+
